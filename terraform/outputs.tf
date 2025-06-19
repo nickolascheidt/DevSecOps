894afb854adca98a970f1289f50412c63b9d57bc
nickolas <nickolas.scheidt@gmail.com>
2025-06-19 11:45:11 -0300

    display public dns in the end to easy access

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,8 @@
 output "instance_public_ip" {
   value = aws_instance.devsecops_instance.public_ip
 }
+
+output "instance_public_dns" {
+  value = aws_instance.devsecops_instance.public_dns
+  description = "DNS público da instância EC2"
+}
